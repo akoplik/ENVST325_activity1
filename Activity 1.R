98792650930755ffca8a902cb1497912ceee267b
akoplik <akoplik@hamilton.edu>
2024-09-07 22:39:44 +0000

    In-Class Prompts Completed

--- Activity 1.R
+++ Activity 1.R
@@ -1,33 +1,19 @@
-# make a vector of numbers
-# elevation in ft
-peaks <- c(5344,5114,4960)
+# In-class prompts
 
-# convert to meters
-peaks/3.281
+# Prompt 1: The average snow depth over the past several days was 2.5, 3, 5, and 4.5 inches. Create a vector of observations and convert these observations to centimeters.
+avg_snow_depth_in <- c(2.5,3.5,4.5)
+avg_snow_depth_cm <- avg_snow_depth_in * 2.54 # 1 in = 2.54 cm
 
-# prominence in ft
-prom <- c(4914,2100,840)
-# difference between height and prominence
-peaks - prom
+# Prompt 2: Below is a table of several of the most difficult high peaks to summit. Enter the information in the table into a data frame.
+highPeaks <- data.frame(name = c("Mount Haystack",
+                                 "Mount Skylight",
+                                 "Dix Mountain",
+                                 "Gray Peak"),
+                        elev = c(4960, 4926, 4857, 4840),
+                        ascent = c(3570, 4265, 2800, 4178),
+                        length = c(17.8, 17.9, 13.2, 16))
 
-# quotes denote a character data type
-peakNames <- c("Mount Marcy", "Algonquin Peak", "Mount Haystack")
+# Prompt 3: Practice mathematical operations in R. Convert the following observations from Celsius to Fahrenheit: -44, 0, 20, 35.
 
-# elevation of first peak
-peaks[1]
-
-# make a data frame
-# you must include the column name = data vector
-# separating multiple columns with commas
-highPeaks <- data.frame(elev = peaks,
-                        prom = prom,
-                        name = peakNames)
-
-# subset 2 row in highPeaks
-highPeaks[2,]
-
-# view only the names column
-highPeaks[,3]
-
-# look at elevation for 3rd highest mountain
-highPeaks[1,3]
+celsius <- c(-44, 0, 20, 35)
+fahrenheit <- (celsius * 9/5) + 32 # °F = (0°C * 9/5) + 32
